007e7ae5de46f3ed879120244c68749a52cc2179
dilushi sewwandi <dilushisewwandi89@gmail.com>
2025-11-10 11:58:46 +0530

    feat(auth): add User Model

---
+++ server/models/UserModel.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
+
+// Define the User model
+export const User = sequelize.define(
+  "User", 
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true, 
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true, 
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "users", 
+    timestamps: true, 
+  }
+);
