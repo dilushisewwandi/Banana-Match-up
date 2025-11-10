455ac3dfa7184c6b1366199470348abca38b959c
dilushi sewwandi <dilushisewwandi89@gmail.com>
2025-11-10 13:49:22 +0530

    chore: add syncModels.js to initialize Sequelize tables and update backend setup

---
+++ server/syncModels.js
@@ -0,0 +1,17 @@
+import { sequelize } from "./config/db.js"; 
+import { User } from "./models/UserModel.js"; 
+
+const syncModels = async () => {
+  try {
+    await sequelize.sync({ force: true }); // drop & recreate tables
+    console.log("All models synced successfully!");
+    console.log("Tables created:", Object.keys(sequelize.models));
+
+    process.exit(0); 
+  } catch (err) {
+    console.error("Error syncing models:", err);
+    process.exit(1); 
+  }
+};
+
+syncModels();
