cea029daad0d6ba30526a509db3141d727ec2bb3
dilushi sewwandi <dilushisewwandi89@gmail.com>
2025-11-11 17:14:23 +0530

    feat(server): register user routes and integrate protected user endpoint

--- server/server.js
+++ server/server.js
@@ -1,26 +1,8 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import authRoutes from "./routes/authRoutes.js"
-
-// dotenv.config();
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/auth", authRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
@@ -35,6 +17,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes)
 
 // Root route for testing
 app.get("/", (req, res) => {
